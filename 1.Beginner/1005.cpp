579addba34167f7fefaa546ec62be549f94d3bf9
AfsarHossain <afsar291722@gmail.com>
2021-10-14 00:21:10 +0600

    1005 No Solution Added

---
+++ 1.Beginner/1005.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double A, B, media;
+    cin>>A>>B;
+    media = ((A*3.5)+(B*7.5))/(3.5+7.5);
+
+    cout<<"MEDIA = "<<fixed<<setprecision(5)<<media<<endl;
+}
